e1d0a2dd740eb483fd4ae3dc209f9f87f80c3744
unknown <kayv@hotmail.fr>
2022-08-17 00:08:05 +0200

    lightbox debut

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -2,7 +2,14 @@ function oui() {
   const ouii = document.querySelectorAll(".portfolio__image");
   ouii.forEach((mediaPic) => {
     mediaPic.addEventListener("click", (e) => {
+        const ouin = document.createElement("div")
+        ouin.classList.add("targetPicture")
+        const siuuu = e.target;
+        const newxOui = document.createElement("img")
+        newxOui.setAttribute("src",siuuu)
       console.log(e.target);
+      console.log(newxOui);
+
     });
   });
   console.log("oui");
